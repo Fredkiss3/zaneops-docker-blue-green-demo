c86e19ba5cdff4ecf2476e9ee4dfad0bda3e366c
Adrien KISSIE <fredkiss3@gmail.com>
2024-07-01 02:22:35 +0200

    ♻️ refactor

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,21 @@ export const onRequest: MiddlewareHandler = async function onRequest(
 	{ locals, request },
 	next,
 ) {
-	const { url, method } = request;
-	const { pathname } = new URL(url);
+	const { url, method, headers } = request;
+	const { pathname, searchParams } = new URL(url);
+
 	const timestamp = new Date().toISOString();
 
 	(locals as { timestamp: string }).timestamp = timestamp;
 
 	console.log(
 		`\n\x1b[90m[${timestamp}]\x1b[37m ${method.toUpperCase()} \x1b[33m${pathname}\x1b[37m`,
+	);
+	console.dir(
+		{
+			headers: Object.fromEntries(headers.entries()),
+		},
+		{ depth: null },
 	);
 
 	const response = await next();
